ac221ff76fc06e549b2df3ba84491524b95de850
THẮNG <hoducthangtn2nhvt@gmail.com>
2022-06-19 11:31:27 +0700

    portfolio

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,13 @@
-import { AiOutlineMail } from "react-icons/ai";
+import {
+  AiOutlineMail,
+  AiOutlineMessage,
+  AiOutlinePhone,
+} from "react-icons/ai";
 import React, { useRef } from "react";
 import emailjs from "emailjs-com";
 import "./contact.css";
 
 function Contact() {
-
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
@@ -40,7 +43,7 @@ function Contact() {
             <a href="mailto:hoducthangtn2nhvt@gmail.com">Send an email</a>
           </article>
           <article className="contact__option">
-            <AiOutlineMail className="contact__option-icon" />
+            <AiOutlineMessage className="contact__option-icon" />
             <h4>Message</h4>
             <h5>Đức Thắng</h5>
             <a href="https://m.me/profile.php?id=100016924470551">
@@ -48,7 +51,7 @@ function Contact() {
             </a>
           </article>
           <article className="contact__option">
-            <AiOutlineMail className="contact__option-icon" />
+            <AiOutlinePhone className="contact__option-icon" />
             <h4>Phone</h4>
             <h5>+8436 592 0074</h5>
           </article>
